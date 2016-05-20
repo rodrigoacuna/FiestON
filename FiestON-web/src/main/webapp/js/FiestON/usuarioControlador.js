689f626f3a88b1b28be34415ae675ae8db6ecad6
nvolpe <nvolpe@IBM700-PC08JA3W>
2016-05-20 11:30:10 -0300

    sadsdsadsa

--- FiestON-web/src/main/webapp/js/FiestON/usuarioControlador.js
+++ FiestON-web/src/main/webapp/js/FiestON/usuarioControlador.js
@@ -3,26 +3,27 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-angular.module('fiestonApp',[]).controller('userController',['$scope','service', function($scope, servicio){
-    
-    $scope.userList = [];
-    
-    function getUserList($scope, $http) {
-    $http.get('localhost:8080/fieston/api/services/getLisUser').
-        success(function(data) {
-            this.userList = data;
-        });
-    }
-    
-        /*$scope.userList = [
-            {usu:"rodri_usuario",clave:"rodri",mail:"mail@mail.com", isChecked:false},
-            {usu:"cateaaaa",clave:"cata",mail:"cata@mail.com", isChecked:true},
-            {usu:"nicooooo",clave:"nico",mail:"nico@mail.com", isChecked:false}
-        ];*/
-    
-    /*self.getUserArray = function(){
-        return self.model.usuarios;
-    }*/
-            
-}]);
+angular.module('fiestonApp', []).controller('userController', ['$scope', function ($scope) {
+
+        //$scope.userList = [];
+
+
+        /*function getUserList($scope, $http) {
+            $http.get('http://localhost:8080/FiestON-web/usuarios').
+                    success(function (data) {
+                        this.userList = data;
+                    });
+        }*/
+
+        $scope.userList = [
+            {usu: "rodri_usuario", clave: "rodri", mail: "mail@mail.com", isChecked: false},
+            {usu: "cateaaaa", clave: "cata", mail: "cata@mail.com", isChecked: true},
+            {usu: "nicooooo", clave: "nico", mail: "nico@mail.com", isChecked: false}
+        ];
+
+        /*self.getUserArray = function(){
+         return self.model.usuarios;
+         }*/
+
+    }]);
 
